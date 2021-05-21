a15c9c36dda5318380d0e22f1d55298ac877d0d9
Everton Marcondes de Oliveira <1322115@ises.com.br>
2021-05-21 08:26:36 -0300

    Add project files.

---
+++ MVVM_NoSql/MVVM_NoSql/View/AlunoView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVVM_NoSql.ViewModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MVVM_NoSql.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AlunoView : ContentPage
+    {
+        AlunoViewModel vmAluno;
+
+        public AlunoView()
+        {
+            var aluno = AlunoViewModel.GetAluno();
+            vmAluno = new AlunoViewModel(aluno);
+            BindingContext = vmAluno;
+
+            InitializeComponent();
+        }
+    }
+}
